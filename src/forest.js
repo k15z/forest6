7714a5c26309fae6217872f00597e425f8ec30d4
Kevin Zhang <kevin.2015.zhang@gmail.com>
2016-02-06 09:14:39 -0500

    V8 optimizations + comments

--- src/forest.js
+++ src/forest.js
@@ -1,6 +1,49 @@
+/**
+ * This is a random forest implementation which supports numerical input vectors
+ * and returns a sorted array of label-probability pairs. Models can be exported
+ * and imported as JSON strings.
+ *
+ * See https://github.com/k15z/forest6 for examples.
+ */
 function Forest (model) {
     var model = model ? model : [];
 
+    /**
+     * Delete useless values left over from training and attach an id and date
+     * value to the model for record-keeping purposes.
+     */
+    function toJSON () {
+        for (var m = 0; m < model.length; m++) {
+            var tree = model[m];
+            var queue = [];
+            queue.push(tree);
+            while (queue.length > 0) {
+                var node = queue.shift();
+                for (var key in node) {
+                    if (key == 'index' || key == 'threshold')
+                        continue;
+                    if (key == 'left' || key == 'right')
+                        continue;
+                    if (key == 'leaf' || key == 'label')
+                        continue;
+                    delete node[key];
+                }
+                if (node.left)
+                    queue.push(node.left)
+                if (node.right)
+                    queue.push(node.right)
+            }
+        }
+        return {
+            id: (Math.random().toString(36)+'00000000000000000').slice(2, 5),
+            date: (new Date()).getTime(),
+            model: model
+        };
+    }
+
+    /**
+     * Take an input data vector and return an array of label-probability pairs.
+     */
     function predict (data) {
         var total = 0;
         var count = {};
@@ -29,11 +72,16 @@ function Forest (model) {
         return probability;
     }
 
+    /**
+     * Take a 2d array composed of N examples of input vectors, N labels, and an
+     * optional options hash. Empty out existing model and build a new model by
+     * finding splits that maximize information gain.
+     */
     function train (data, label, options) {
         var options = {
-            depth: ((options && options.depth) ? options.depth : 2),
-            tries: ((options && options.tries) ? options.tries : 4),
-            trees: ((options && options.trees) ? options.trees : 8)
+            depth: ((options && options.depth) ? options.depth : 4),
+            tries: ((options && options.tries) ? options.tries : 16),
+            trees: ((options && options.trees) ? options.trees : 256)
         }
 
         model.length = 0;
@@ -58,7 +106,7 @@ function Forest (model) {
                             var k = Math.random();
                             var d1 = node.all_ix[_randi(node.all_ix.length)];
                             var d2 = node.all_ix[_randi(node.all_ix.length)];
-                            if (d2 == d1) d2 = node.all_ix[_randi(node.all_ix.length)];
+                            while (d2 == d1) d2 = node.all_ix[_randi(node.all_ix.length)];
                             return data[d1][index]*k + data[d2][index]*(1-k);
                         })();
 
@@ -97,6 +145,9 @@ function Forest (model) {
         }
     }
 
+    /**
+     * Return random integer between low (inclusive) and high.
+     */
     function _randi (low, high) {
         if (high == undefined) {
             high = low;
@@ -105,6 +156,9 @@ function Forest (model) {
         return Math.floor(Math.random() * (high - low) + low);
     }
 
+    /**
+     * Return an array filled with [start, start+1, start+2, ... end-1] values.
+     */
     function _range (start, end) {
         if (end == undefined) {
             end = start;
@@ -117,19 +171,28 @@ function Forest (model) {
         return arr;
     }
 
+    /**
+     * Return a histogram-like object indicating the frequency of each label in
+     * the subset of labels marked by the indices in ix.
+     */
     function _count (label, ix) {
         if (!ix)
-            ix = _range(label.length)
+            ix = _range(0,label.length)
 
+        var i;
         var count = {};
-        ix.forEach(function(i) {
+        for (var x = 0; x < ix.length; x++) {
+            i = ix[x];
             if (!count[label[i]])
                 count[label[i]] = 0;
             count[label[i]]++;
-        });
+        }
         return count;
     }
 
+    /**
+     * Return the entropy of the subset of labels marked by the indices in ix.
+     */
     function _entropy (label, ix) {
         if (!ix)
             ix = _range(label.length)
@@ -145,13 +208,14 @@ function Forest (model) {
         for (var key in count)
             if (count.hasOwnProperty(key)) {
                 prob = count[key]/total;
-                entropy += prob * Math.log2(prob);
+                entropy += prob * Math.log(prob);
             }
         return -entropy;
     }
 
     var exports = {_util: {}};
     exports.model = model;
+    exports.toJSON = toJSON;
     exports.train = train;
     exports.predict = predict;
     exports._util._randi = _randi;
